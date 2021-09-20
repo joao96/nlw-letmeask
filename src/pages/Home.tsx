322cea51ba86ca9147c8810ddd94d8e64fb3f733
joao96 <jvpoletti@gmail.com>
2021-09-20 15:51:05 -0300

    ⚡️ adding auth hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,13 +6,13 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 
+import { useAuth } from '../hooks/useAuth';
+
 import '../styles/auth.scss';
-import { useContext } from 'react';
-import { AuthContext } from '../App';
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useContext(AuthContext);
+  const { user, signInWithGoogle } = useAuth();
 
   async function handleCreateRoom() {
     if (!user) {
