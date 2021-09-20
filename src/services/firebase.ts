a244e7a8f320f451c7dd20e463a9f46dd36602b1
joao96 <jvpoletti@gmail.com>
2021-09-20 14:08:37 -0300

    🎨 adding home structure

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID
 };
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth();
-export const database = getDatabase();
+export const auth = getAuth(app);
+export const database = getDatabase(app);
